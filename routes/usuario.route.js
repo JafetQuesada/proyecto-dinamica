fe9b7128171c925b5d3536cc58deab791636f0f2
JafetQuesada <73149973+JafetQuesada@users.noreply.github.com>
2021-03-09 09:40:42 -0600

    inicio  sesion

--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -86,6 +86,37 @@ router.put('/modificar-usuario', (req, res) => {
     });
 
 });
+router.post('/iniciar-sesion', (req, res) => {
+    let correo = req.body.correo;
+    let contrasenna = req.body.contrasenna;
+
+    Usuario.findOne({ correo: correo }, (err, usuario) => {
+        if (err) {
+            res.json({
+                msj: 'El correo electrónico o la contraseña no son correctos',
+                inicio: false,
+                err
+            });
+        } else {
+            if (usuario && usuario.contrasenna == contrasenna) {
+                res.json({
+                    correo: usuario.correo,
+                    tipo: usuario.tipo,
+                    nombre: usuario.nombre,
+                    inicio: true
+                });
+            } else {
+                res.json({
+                    msj: 'El correo electrónico o la contraseña no son correctos',
+                    inicio: false,
+                    err
+                });
+            }
 
 
+        }
+    });
+
+
+});
 module.exports = router;
